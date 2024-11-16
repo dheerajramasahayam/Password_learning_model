fccd7fde2d7db75d96d9cae7d937550d62fef42b
Dheeraj Ramasahayam <dheerajramasahayam@gmail.com>
2024-11-16 13:07:47 -0500

    Made changes to main.py

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ char_space = list("abcdefghijklmnopqrstuvwxyz0123456789")  # Lowercase + numbers
 # Path to your wordlist (e.g., rockyou.txt)
 wordlist_path = "rockyou.txt"  # Change this to the path of your wordlist file
 
+# Select a random password from the wordlist as target_password
+import random
+wordlist = open(wordlist_path, 'r', encoding='utf-8', errors='ignore').readlines()
+target_password = random.choice(wordlist).strip()
+
 # Train the model
-train_model(target_password=None, char_space=char_space, wordlist_path=wordlist_path)
+train_model(target_password=target_password, char_space=char_space, wordlist_path=wordlist_path)
